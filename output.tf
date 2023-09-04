af422b4cee17f062ae5b6f58e07cc2c06fdc7459
Rod Stewart <rodstewart@gmail.com>
2023-09-03 23:29:27 -0500

    feat: Add descriptions to outputs

--- output.tf
+++ output.tf
@@ -1,10 +1,13 @@
 output "bastion_name" {
-  value = azurerm_bastion_host.bastion.name
+  value       = azurerm_bastion_host.bastion.name
+  description = "The name of the deployed Bastion Host."
 }
 
 output "bastion_dns_name" {
-  value = azurerm_bastion_host.bastion.dns_name
+  value       = azurerm_bastion_host.bastion.dns_name
+  description = "The DNS hostname of the deployed Bastion Host."
 }
 output "bastion_out" {
-  value = azurerm_bastion_host.bastion
+  value       = azurerm_bastion_host.bastion
+  description = "The complete output of the Bastion Host."
 }
